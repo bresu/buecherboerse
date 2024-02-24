7393e2d28ad3825a46b0a8736dc65dce0c003ed0
Matteioo <26168940+matteioo@users.noreply.github.com>
2024-02-24 13:41:58 +0100

    Fixed typo in language code.

--- buecherboerse/settings.py
+++ buecherboerse/settings.py
@@ -118,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'de-at'
+LANGUAGE_CODE = 'de_AT'
 
 TIME_ZONE = 'UTC'
 
