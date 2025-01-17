68ee7965968a62f94a40a65ba0c9aac96f42a269
Matteo <26168940+matteioo@users.noreply.github.com>
2025-01-17 20:20:18 +0100

    Add optional superuser creation

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
